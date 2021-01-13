6b63b7f8e96e1a1358d71845aa1c80cba157cb5a
SebGrd <sebastien.gaudard.1@gmail.com>
2021-01-13 22:46:49 +0100

    feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div id="app">
-    <header id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/experience">experience</router-link> |
-      <router-link to="/skills">skills</router-link> |
-      <router-link to="/schools">schools</router-link> |
-      <router-link to="/projects">projects</router-link> |
-      <router-link to="/contact">contact</router-link>
-    </header>
+    <Header/>
     <main>
       <router-view/>
     </main>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header";
+export default {
+  components: {Header}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap');
+
+*{
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Nunito, Ubuntu, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="site-header" id="header">
+    <a href="/" class="site-header__title"><b>Sébastien Gaudard</b></a>
+    <nav class="site-header__nav">
+      <ul class="site-header__nav__list">
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/">🏠</router-link>
+        </li>
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/experience">Experience</router-link>
+        </li>
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/skills">Skills</router-link>
+        </li>
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/schools">Schools</router-link>
+        </li>
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/projects">Projects</router-link>
+        </li>
+        <li class="site-header__nav__list__item">
+          <router-link class="site-header__nav__list__item__link" to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+//router-link-active = also parents
+.router-link-exact-active{
+  color: yellow;
+}
+</style>
