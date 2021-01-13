04eeadc25285e023eb56a4ced3c867fa7267f6cd
SebGrd <sebastien.gaudard.1@gmail.com>
2021-01-13 22:53:27 +0100

    feat: autogenerate menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,23 +3,8 @@
     <a href="/" class="site-header__title"><b>Sébastien Gaudard</b></a>
     <nav class="site-header__nav">
       <ul class="site-header__nav__list">
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/">🏠</router-link>
-        </li>
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/experience">Experience</router-link>
-        </li>
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/skills">Skills</router-link>
-        </li>
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/schools">Schools</router-link>
-        </li>
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/projects">Projects</router-link>
-        </li>
-        <li class="site-header__nav__list__item">
-          <router-link class="site-header__nav__list__item__link" to="/contact">Contact</router-link>
+        <li class="site-header__nav__list__item" v-for="(route, index) in this.$router.options.routes" :key="index">
+          <router-link class="site-header__nav__list__item__link" :to="route.path">{{ route.name }}</router-link>
         </li>
       </ul>
     </nav>
@@ -28,13 +13,13 @@
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
 }
 </script>
 
 <style scoped lang="scss">
 //router-link-active = also parents
-.router-link-exact-active{
+.router-link-exact-active {
   color: yellow;
 }
 </style>
